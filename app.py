9c65d66b73153115d1039af93b54a13ba43a306f
SpartanKurt051 <37214287+SpartanKurt051@users.noreply.github.com>
2025-04-30 17:19:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def generate_pie_chart(column_name, df):
     grouped_data = df[column_name].value_counts().reset_index()
     grouped_data.columns = [column_name, 'Count']
     fig = px.pie(grouped_data, names=column_name, values='Count')
-    fig.update_traces(textinfo='percent+label', textposition='inside') # Percentages inside the chart
+    fig.update_traces(textinfo='percent', textposition='inside') # Percentages inside the chart
     fig.update_layout(
             paper_bgcolor="black",
             plot_bgcolor="black",
